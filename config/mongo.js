15c703d78c1909e4900d34657c3dabd48aba55db
Amaan1432 <amaanahmad00@gmail.com>
2023-09-24 20:23:06 +0530

    final touch on mongoDB

--- config/mongo.js
+++ config/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 // const dbConnection=mongoose.connect('mongodb://127.0.0.1:27017/IM-Interview');
-
-const dbConnection=mongoose.connect('mongodb+srv://amaan1432:jpladiFXLD1wtEPD@cluster0.wqiwl1y.mongodb.net/IM-Interviewy?retryWrites=true&w=majority')
+const url = 'mongodb+srv://amaan1432:jpladiFXLD1wtEPD@cluster0.wqiwl1y.mongodb.net/IM-Interviewy?retryWrites=true&w=majority';
+const dbConnection=mongoose.connect(url)
 .then(()=>{
     console.log('Connection successful!');
 }).catch((err) => console.log("no connection " + err));
